800faa3dc6d866a4cf40e86a2563b710f0ef9c3e
Eric Gutierrez Jr <17133321+CireWire@users.noreply.github.com>
2023-10-27 12:03:19 -0700

    Create WaveConfig.cs

---
+++ WaveConfig.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class WaveConfig
+{
+    public GameObject enemyPrefab;
+    public int enemyCount;
+    public float timeBetweenSpawns;
+}
